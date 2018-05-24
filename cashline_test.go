a49889618999194ec5a73d9dec105fd2d717ca18
Diego Savardi <savardiego@users.noreply.github.com>
2018-05-25 00:00:52 +0200

    Some more test to finish on hshing

--- cashline_test.go
+++ cashline_test.go
@@ -8,6 +8,10 @@ import (
 )
 
 func TestTestVectors(t *testing.T) {
+	text := "tanto va la gatta al lardo che ci lascia lo zampino"
+	end := "firma"
+	full := text + end
+
 	hasher := sha256.New()
 	if hasher == nil {
 		t.Errorf("Sha256 is nil")
@@ -15,18 +19,14 @@ func TestTestVectors(t *testing.T) {
 	// hasher.Write([]byte("cogito ergo sum"))
 	fmt.Printf("Blocksize: %v\n", hasher.BlockSize())
 	fmt.Printf("Size: %v\n", hasher.Size())
-	hashsum := hasher.Sum([]byte("cogito ergo sum"))
+	hashfull := hasher.Sum([]byte(full))
+	encoded := base64.StdEncoding.EncodeToString(hashfull)
+	fmt.Printf("Encoded: %v\n", encoded)
 
-	encoded := base64.StdEncoding.EncodeToString(hashsum)
-	fmt.Println(encoded)
-
-	// if !ok {
-	// 	t.Error("Marshaler dont instantiate")
-	// }
-	// state, err := marshaler.MarshalBinary()
-	// if err != nil {
-	// 	t.Error("Marshaler does not work")
-	// }
-	fmt.Printf("Sum: %x\n", hashsum)
+	hasher.Reset()
+	hasher.Write([]byte(text))
+	hashend := hasher.Sum([]byte(end))
+	encodedE := base64.StdEncoding.EncodeToString(hashend)
+	fmt.Printf("Encoded: %v\n", encodedE)
 	fmt.Println("THE END")
 }
